002bcc214e73e89a859b5156e0e89431d1e82f91
is2 <duishenov.islam1@gmail.com>
2024-11-04 14:29:13 +0600

    commit

--- dto/src/main/java/com/dto/dto/configs/DataInitializer.java
+++ dto/src/main/java/com/dto/dto/configs/DataInitializer.java
@@ -2,6 +2,7 @@ package com.dto.dto.configs;
 
 import com.dto.dto.model.Role;
 import com.dto.dto.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataInitializer {
 
+    @Autowired
+    RoleRepository roleRepository;
     @Bean
     public CommandLineRunner loadRoles(RoleRepository roleRepository) {
         return args -> {
-                roleRepository.save(new Role(null, "ROLE_USER", null));
-
-                roleRepository.save(new Role(null, "ROLE_ADMIN", null));
+                if (!roleRepository.findByName("ROLE_USER").equals("ROLE_USER")) {
+                    roleRepository.save(new Role(null, "ROLE_USER", null));
+                }
+                if (!roleRepository.findByName("ROLE_ADMIN").equals("ROLE_ADMIN")) {
+                    roleRepository.save(new Role(null, "ROLE_ADMIN", null));
+                }
 
         };
     }
